e45bcc4f072c71500f271e97da7aab8062ff71c1
corinne <160467276+coryxn@users.noreply.github.com>
2024-03-08 10:35:31 +0000

    final submission

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 nric = input('Enter an NRIC number: ')
-
-# Type your code below
+nric = nric.upper()
+if not nric.startswith('S') and nric.startswith('T') and nric.startswith('F') and nric.startswith('G'):
+  print('NRIC is invalid.')
+elif not nric[1:-2].isdecimal():
+  print('NRIC is invalid.')
+elif not nric[-1].isalpha():
+  print('NRIC is invalid.')
+elif len(nric) != 9:
+  print('NRIC is invalid.')
+else:
+  num = int(nric[1])*2 + int(nric[2])*7 + int(nric[3])*6  + int(nric[4])*5 + int(nric[5])*4 + int(nric[6])*3 + int(nric[7])*2
+  if nric[0] == ('T' or 'G'):
+    num = int(num) + 4
+  else: 
+    num = int(num)
+  quotient, remainder = divmod(num, 11)
+  if nric[0].startswith('S') or nric[0].startswith('T'):
+    list = ['J', 'Z', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A']
+  else: 
+    list = ['X', 'W', 'U', 'T', 'R', 'Q', 'P', 'N', 'M', 'L', 'K']
+  if list[remainder] == nric [-1]:
+    print('NRIC is valid.')
+  else:
+    print('NRIC is invalid.')
